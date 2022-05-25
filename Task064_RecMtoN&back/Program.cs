f07d1239489395bc20d020acad6b8fe25b384c2c
helenfromboston <helenfromboston@gmail.com>
2022-05-25 18:09:38 +0300

    First commit

---
+++ Task064_RecMtoN&back/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5" M = 4; N = 8 -> "4, 5, 6, 7, 8"
+// И наоборот, если M больше N.
+
+void Rec(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+    Console.Write(M + " ");
+    Rec(M + 1, N);
+}
+
+void RecBack(int M, int N)
+{
+    if (M < N)
+    {
+        return;
+    }
+    Console.Write(M + " ");
+    RecBack(M - 1, N);
+}
+
+Console.WriteLine("Введите M и N: ");
+int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+if (M < N) Rec(M, N);
+else RecBack(M, N);
---
+++ Task068_Ackermann/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int m = 3;
+int n = 5;
+int A = 0;
+
+int RecAck(int m, int n)
+{
+    if (m == 0)
+    {
+        return A = n + 1;
+    }
+    if (n == 0)
+    {
+        return A = RecAck(m - 1, 1);
+    }
+    else
+    {
+        return A = RecAck(m - 1, RecAck(m, n - 1));
+    }
+}
+
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Функция Аккермана определяется только для неотрицательных целых чисел");
+}
+else
+{
+    RecAck(m, n);
+    Console.WriteLine(A);
+}
